b84d049ea4281bf412e96968400fd0fb9aa6f393
viktor7reut <viktor7reut@gmail.com>
2024-03-03 21:52:01 +0300

    savePoint1
    startHW

---
+++ Lesson11HW/Lesson11HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson11HW
+//
+//  Created by Виктор Реут on 3.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
